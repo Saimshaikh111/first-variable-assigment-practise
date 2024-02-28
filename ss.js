01d2ab689abc2b40a04267bdfbb52c2139bc468a
Saimshaikh111 <160360411+Saimshaikh111@users.noreply.github.com>
2024-02-28 16:06:26 +0500

    Add files via upload

---
+++ ss.js
@@ -0,0 +1,25 @@
+//Name
+var name6 = "hello saim ";
+console.log(name6);
+//father name
+var name7 = "hello dilshad ali";
+console.log(name7);
+//age
+var age1 = 20;
+console.log(age1);
+//address
+var name8 = "near samama";
+console.log(name8);
+//contact
+var contact = 0;
+console.log(contact);
+//nationality
+var name9 = "pakistani";
+console.log(name9);
+//isMarried
+var ISmarried = false;
+console.log(ISmarried);
+var abc1 = undefined;
+console.log(undefined);
+var xyc = null;
+console.log(null);
---
+++ ss.ts
@@ -0,0 +1,25 @@
+//Name
+let name6:string = "hello saim "
+console.log(name6) 
+//father name
+let name7: string ="hello dilshad ali"
+console.log(name7)
+//age
+let age1 : number = 20
+console.log(age1)
+//address
+let name8 : string = "near samama"
+console.log(name8)
+//contact
+let contact: number = 0
+console.log(contact)
+//nationality
+let name9: string= "pakistani"
+console.log(name9)
+//isMarried
+let ISmarried :boolean= false
+console.log(ISmarried) 
+let abc1: undefined=undefined
+console.log(undefined)
+let xyc: null=null
+console.log(null)
